44ebaf7de685f0121e558227d32e4a4894163be2
IsayMeneses <136571350+IsayMeneses@users.noreply.github.com>
2023-07-05 21:05:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
 
-// Configure body-parser middleware to handle JSON data
+const app = express();
 app.use(bodyParser.json());
 
-// Define the save route
-app.post('/save', (req, res) => {
-  const { student, date, html } = req.body;
+const dataDir = path.join(__dirname, "data");
 
-  // Save the table data to a file
-  const fileName = `student_reports/${student}_${date}.html`;
-  const filePath = path.join(__dirname, fileName);
-  fs.writeFile(filePath, html, (error) => {
-    if (error) {
-      console.error('Error saving table:', error);
-      res.status(500).json({ message: 'Failed to save table.' });
+app.post("/save", (req, res) => {
+  const date = new Date().toISOString().split("T")[0];
+  const filePath = path.join(dataDir, `${date}.json`);
+  const jsonData = JSON.stringify(req.body);
+
+  fs.writeFile(filePath, jsonData, (err) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
     } else {
-      res.status(200).json({ message: 'Table saved successfully.' });
+      res.sendStatus(200);
     }
   });
 });
 
-// Serve static files
-app.use(express.static('public'));
-
-// Start the server
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
